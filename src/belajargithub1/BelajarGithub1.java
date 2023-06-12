7c3ae99ee74417a36749805fdf7bb0267edf3eca
YudhiWahyu <wahyunoma@gmail.com>
2023-06-12 21:28:19 +0800

    tanggal dan waktu

--- src/belajargithub1/BelajarGithub1.java
+++ src/belajargithub1/BelajarGithub1.java
@@ -1,14 +1,22 @@
 package belajargithub1;
 
+import java.util.Calendar;
+
 public class BelajarGithub1 {
     public static void main(String[] args) {
-String str = "Wahyudhi Noma H";
 
-String str1 = str.substring (0,5);
-String str2 = str.substring (3,8);
+Calendar c = Calendar.getInstance();        
+        
+        String str = "Wahyudhi Noma H";
+        String str1 = str.substring (0,5);
+        String str2 = str.substring (3,8);
+            System.out.println("Nama Depan : " + str1);
+            System.out.println("Nama Depan : " + str2);
+        
 
-        System.out.println("Nama Depan : " + str1);
-        System.out.println("Nama Depan : " + str2);
+System.out.format("%tB %te, %tY%n", c, c, c);
+System.out.format("%tl:%tM %tp%n", c, c, c);
+System.out.format("%tD%n", c);
     }
     
 }
